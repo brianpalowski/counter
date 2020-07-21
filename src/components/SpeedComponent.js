fcd24e4e0f19c066f92425092185354c39795ddf
Brian Palowski <brianpalowski@Brians-MacBook-Pro.local>
2020-07-21 02:42:11 -0700

    push

--- src/components/SpeedComponent.js
+++ src/components/SpeedComponent.js
@@ -1,10 +1,8 @@
 import React from 'react'
-
 const SpeedComponent = ({ increaseMethod }) => {
 
   return (
     <div className="speed-item">
-
       <button onClick={() => increaseMethod(1000)} >1x</button>
       <button onClick={() => increaseMethod(1500)}>1.5x</button>
       <button onClick={() => increaseMethod(2000)} >2x</button>
